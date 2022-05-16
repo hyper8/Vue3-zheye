a4b983831e90049cba30e1f1e0d952d0bfb4aa72
hyper8 <523354015@qq.com>
2022-05-16 16:53:28 +0800

    初始化column组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app" class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+const testdata: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'testzhuanlan',
+    avatar: 'https://cdn4.buysellads.net/uu/1/81016/1609783170-authentic-260x200-variation-1.jpg',
+    description: 'zhuanlan de miaoshu'
+  },
+  {
+    id: 2,
+    title: 'testzhuanlan',
+    avatar: 'https://cdn4.buysellads.net/uu/1/81016/1609783170-authentic-260x200-variation-1.jpg',
+    description: 'zhuanlan de miaoshu'
+  },
+  {
+    id: 3,
+    title: 'testzhuanlan',
+    avatar: 'https://cdn4.buysellads.net/uu/1/81016/1609783170-authentic-260x200-variation-1.jpg',
+    description: 'zhuanlan de miaoshu'
+  },
+  {
+    id: 4,
+    title: 'testzhuanlan',
+    avatar: 'https://cdn4.buysellads.net/uu/1/81016/1609783170-authentic-260x200-variation-1.jpg',
+    description: 'zhuanlan de miaoshu'
+  }
+
+]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testdata
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

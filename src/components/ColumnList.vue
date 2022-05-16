54e1a975715d9f3639b481a95d1db3d9f52d0c24
hyper8 <523354015@qq.com>
2022-05-16 17:13:33 +0800

    美化CSS样式(可能)

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,10 +1,14 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title">
-      <h3>{{column.title}}</h3>
-      <p>{{column.description}}</p>
-      <a href="#">进入专栏</a>
+  <ul class="row">
+    <li class="col-4 mb-3" v-for="column in list" :key="column.id">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img class="rounded-circle border border-light w-25 y-3" :src="column.avatar" :alt="column.title">
+          <h3 class="card-title">{{column.title}}</h3>
+          <p class="card-text text-left">{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
     </li>
   </ul>
 </template>
